2de38bb633add7c1682b0c4c94ffe151f6d3f77e
Chellsi <alexsarnavskiy@gmail.com>
2025-08-14 13:09:40 +0300

    init

---
+++ my-redux-app/src/App.tsx
@@ -0,0 +1,33 @@
+// src/App.tsx
+import './App.css'
+import { ControlledForm } from './components/ControlledForm'
+import { UncontrolledForm } from './components/UncontrolledForm'
+import { UsestateFetch } from './components/UsestateFetch'
+import { useAppDispatch, useAppSelector } from './redux/hooks'
+import { toggleControlledForm } from './redux/slices/visibilitySlice'
+
+function App() {
+  const dispatch = useAppDispatch()
+  const isVisible = useAppSelector(state => state.visibility.isControlledFormVisible)
+
+  const handleClick = () => {
+    dispatch(toggleControlledForm())
+  }
+
+  return (
+    <>
+      <div className="flex flex-col gap-4 items-center justify-center bg-gray-100 p-4 dark:bg-gray-900">
+        <button onClick={handleClick} className="bg-blue-500 rounded-md p-2">Change Status</button>
+        {isVisible && <ControlledForm/>}
+      </div>
+      <div className="flex flex-col gap-4 items-center justify-center bg-gray-100 p-4 dark:bg-gray-900">
+        <UncontrolledForm />
+      </div>
+      <div className="flex flex-col gap-4 items-center justify-center bg-gray-100 p-4 dark:bg-gray-900">
+        <UsestateFetch />
+      </div>
+    </>
+  )
+}
+
+export default App
